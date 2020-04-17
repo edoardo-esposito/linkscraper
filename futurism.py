24e34101438112694691b8cb13426b1f653463c6
Edoardo Esposito <edoardo.esposito@gmail.com>
2020-04-17 17:18:28 +0200

    Minor fixes on futurism

--- futurism.py
+++ futurism.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import re
 from abstractscraper import AbstractScraper
 
+
 class Futurism_Scraper(AbstractScraper):
 
     def get_items(self):
@@ -13,16 +14,16 @@ class Futurism_Scraper(AbstractScraper):
 
         headers = super().set_headers()
         results = requests.get(self.url.strip(), headers=headers)
-        
-        soup = BeautifulSoup(results.text, "html.parser")
-        container = soup.find('div', class_ = 'container')
 
-        titles = soup.find_all('h4', class_ = 'card-title')
+        soup = BeautifulSoup(results.text, "html.parser")
+        container = soup.find('div', class_='container')
+
+        titles = soup.find_all('h4', class_='card-title')
 
         links = []
         for obj in titles:
             titolo = obj.get_text()
-            links.append({ 
+            links.append({
                 'id': hashlib.md5(titolo.encode('utf-8')).hexdigest(),
                 'titolo': titolo,
                 'url': obj.find('a').get('href'),
@@ -36,28 +37,29 @@ class Futurism_Scraper(AbstractScraper):
 
         return self.links
 
-    def get_item_text(self,url):
+    def get_item_text(self, url):
         super().get_item_text(url)
 
         headers = super().set_headers()
         results = requests.get(url.strip(), headers=headers)
-        
+
         soup = BeautifulSoup(results.text, "html.parser")
-        content_div = soup.find(attrs={"itemprop" : "articleBody"})
+        content_div = soup.find(attrs={"itemprop": "articleBody"})
 
         text = super().clean_text(content_div)
 
         return text
 
-def run(): 
+
+def run():
+    #TODO move url to generic config file (same for execute.py)
     URL = "https://feeder.co/discover/085f3bc023/futurism-com"
     s = Futurism_Scraper(URL)
     s.get_items()
     items = s.cycle_items()
-    
-    print (items)
+
+    print(items)
+
 
 if __name__ == '__main__':
     run()
-
-
