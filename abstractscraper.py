48db7fd66dc800b0906888bbbc8f66731da347cb
Edoardo Esposito <edoardo.esposito@gmail.com>
2020-04-17 17:09:47 +0200

    Update abstractscraper.py

--- abstractscraper.py
+++ abstractscraper.py
@@ -3,12 +3,13 @@ from urllib.request import urlopen
 import csv
 from bs4 import BeautifulSoup, NavigableString
 import hashlib
-import sqlite3
 from datetime import datetime
 import re
 
 DEBUG = True
 JUST_ONE_LINK = True
+
+
 class AbstractScraper(object):
 
     def __init__(self, url):
@@ -17,22 +18,19 @@ class AbstractScraper(object):
 
     def set_headers(self):
         headers = {
-                "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OSX 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-                'Accept': 'application/xml;q=0.9, */*;q=0.8'
-#                "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" 
-                }
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OSX 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+            'Accept': 'application/xml;q=0.9, */*;q=0.8'
+        }
 
         return headers
 
     def get_items(self):
         if DEBUG:
-            print ("Get links from [%s]" % self.url)
+            print("Get links from [%s]" % self.url)
 
         pass
 
     def cycle_items(self):
-#        if DEBUG:
-#            print ("CYCLE ARTICLES")
 
         for l in self.links:
             text = self.get_item_text(l['url'])
@@ -40,9 +38,9 @@ class AbstractScraper(object):
 
         return self.links
 
-    def get_item_text(self,url):
+    def get_item_text(self, url):
         if DEBUG:
-            print ("Get text from article [%s]" % url.encode())
+            print("Get text from article [%s]" % url.encode())
 
         pass
 
@@ -53,7 +51,7 @@ class AbstractScraper(object):
     def parse_standard_rss(self, url):
         headers = self.set_headers()
 
-        req = urllib.request.Request(self.url, headers = headers)
+        req = urllib.request.Request(self.url, headers=headers)
         parse_xml_url = urllib.request.urlopen(req)
 
         xml_page = parse_xml_url.read()
@@ -68,7 +66,7 @@ class AbstractScraper(object):
             titolo = getfeed.title.text
             link_id = self.generate_link_id(titolo)
 
-            links.append({ 
+            links.append({
                 'id': link_id,
                 'titolo': titolo,
                 'url': getfeed.link.nextSibling.rstrip(),
@@ -83,7 +81,7 @@ class AbstractScraper(object):
     def parse_atom_rss(self, url):
         headers = self.set_headers()
 
-        req = urllib.request.Request(self.url, headers = headers)
+        req = urllib.request.Request(self.url, headers=headers)
         parse_xml_url = urllib.request.urlopen(req)
 
         xml_page = parse_xml_url.read()
@@ -96,7 +94,7 @@ class AbstractScraper(object):
         for getfeed in news_list:
             titolo = getfeed.title.text
 
-            links.append({ 
+            links.append({
                 'id': hashlib.md5(titolo.encode('utf-8')).hexdigest(),
                 'titolo': titolo,
                 'url': getfeed.link['href'],
@@ -109,126 +107,29 @@ class AbstractScraper(object):
         return links
 
     def strip_tags(self, soup, valid_tags):
-#        soup = BeautifulSoup(html, "html.parser")
-    
         for tag in soup.findAll(True):
             if tag.name in valid_tags:
-                print ('-----')
-                print (tag.name)
-                print (tag.contents)
+                print('-----')
+                print(tag.name)
+                print(tag.contents)
                 for t in tag.contents:
-
                     self.strip_tags(t, valid_tags)
 
-                print ('-----\n')
+                print('-----\n')
 
-#                for t in tag.contents:
-#                     print ('-----')
-#                     print (t.name)
-#                     print (t.contents)
-#                     print ('-----\n')
-##                    if t.name in valid_tags:
-##                        print ('-----')
-##                        print (type(t))
-##                        print (t.contents)
-##                        print ('-----\n')
-#
-#
-##                s = ""
-##    
-##                for c in tag.contents:
-##                    if not isinstance(c, NavigableString):
-##                        c = self.strip_tags(c, invalid_tags)
-##                    s += str(c)
-##    
-##                tag.replaceWith(s)
-    
         return soup
-    
+
     def clean_text(self, taggedtext):
-#        if DEBUG:
-#            print ("Clean text [%s]" % taggedtext)
-
-#        allowed_tags = ['h2', 'p']
-#
-#        text = ''
-#        for tag in taggedtext.find_all(allowed_tags):
-#            t = tag.extract()
-#            text += str(t)
-#
-#        btext = BeautifulSoup(text, "html.parser")
-#
-#        if len(btext.find_all('script')) > 0:
-#            btext.script.decompose()
-#
-#        for tag in btext:
-#            for attribute in ['class', 'id', 'name', 'style', 'lang']:
-#                del tag[attribute]
-
-        
-
-#        print ("Lunghezza normale %d" % len(taggedtext.text))
-#        valid_tags = ['h2', 'p']
-#        strippedtext = self.strip_tags(taggedtext, valid_tags)
-#        print ("Lunghezza strippato %d" % len(strippedtext.text))
-
         btext = taggedtext.text
 
         return btext
 
     def clean_paragraph(self, paragraph):
-#        if DEBUG:
-#            print ("Clean paragraph [%s]" % paragraph)
-
         cleantext = paragraph.text
-        cleantext = cleantext.replace(r'\n','')
-        cleantext = cleantext.replace(r'\r','')
-        cleantext = re.sub(r'[^A-Za-z0-9\s]+','', str(cleantext))
+        cleantext = cleantext.replace(r'\n', '')
+        cleantext = cleantext.replace(r'\r', '')
+        cleantext = re.sub(r'[^A-Za-z0-9\s]+', '', str(cleantext))
         cleantext = cleantext.strip()
-
-#        if DEBUG:
-#            print ("Cleaned [%s]" % cleantext)
-
-        return cleantext
-
-    def WIP_clean_text(self, taggedtext):
-        if DEBUG:
-            print ("Clean text [%s]" % taggedtext)
-        
-
-#        btext = taggedtext.text
-
-        allowed_tags = ['h2', 'p']
-        disallowed_tags = ['class', 'id', 'name', 'style', 'lang', 'span']
-        text = ''
-
-#        for tag in taggedtext.find_all(disallowed_tags):
-        for tag in taggedtext.find_all(allowed_tags):
-            print ("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-            print (tag)
-            print ("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
-#            t = tag.extract()
-#            text += str(t)
-
-#        print ("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ------")
-#        print (text)
-#
-#        btext = BeautifulSoup(text, "html.parser")
-#
-#        if len(btext.find_all('script')) > 0:
-#            btext.script.decompose()
-#
-#        for tag in btext:
-#            for attribute in ['class', 'id', 'name', 'style', 'lang', 'span']:
-#                del tag[attribute]
-#        
-        cleantext = taggedtext.text
-#        cleantext = cleantext.replace(r'\n','')
-#        cleantext = cleantext.replace(r'\r','')
-#        cleantext = re.sub(r'[^A-Za-z0-9\s]+','', str(cleantext))
-#
-#        print ("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB ------")
-#        print (cleantext)
 
         return cleantext
 
@@ -237,10 +138,14 @@ class AbstractScraper(object):
             with open('articoli.csv') as csv_file:
                 csv_reader = csv.reader(csv_file, delimiter=';')
                 line_count = 0
-    
+
+                print (word)
                 for row in csv_reader:
+                    print (row[0])
                     if row[0] == word:
                         return True
+        except IndexError:
+            pass
         except IOError:
             print("File non presente")
 
@@ -248,17 +153,17 @@ class AbstractScraper(object):
 
     def items_to_csv(self):
         if DEBUG:
-            print ("Print to CSV")
+            print("Print to CSV")
 
         csv_file = "articoli.csv"
-        csv_columns = ['id','titolo','url','data','text']
+        csv_columns = ['id', 'titolo', 'url', 'data', 'text']
 
-        rows =[]
+        rows = []
         for data in self.links:
 
             if self.find_items_in_csv(csv_file, data['id']):
                 if DEBUG:
-                    print ("Elemento con %s già esistente" % data['id'])
+                    print("Elemento con %s già esistente" % data['id'])
                 continue
 
             row = {}
@@ -267,12 +172,8 @@ class AbstractScraper(object):
             row['url'] = data['url'].rstrip()
             row['data'] = data['data']
             row['text'] = data['text']
-#            row['text'] = data['text'][0:100].decode('UTF-8')
-#           date_obj = datetime.strptime(data['data'], '%a, %d %b %Y %H:%M:%S %Z')
-#           print (date_obj)
-            
+
             rows.append(row)
-#            print (rows)
 
         try:
             with open(csv_file, 'a') as csvfile:
@@ -283,4 +184,3 @@ class AbstractScraper(object):
 
         except IOError:
             print("I/O error")
-
